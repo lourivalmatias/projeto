1403eb736cdd3494ad4105645149a308e896b578
lourivalmatias <lourival_matias@hotmail.com>
2016-07-19 20:50:39 -0300

    Pagina recebendo os arquivos cadastrados

--- envia.php
+++ envia.php
@@ -1,37 +1,81 @@
 <!DOCTYPE html>
-
 <html lang="pt-BR">
 <head>
-	<meta charset="UTF-8">
+	
+	<meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 
 </head>
 <body>
-<?php
-
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$senha = $_POST['senha'];
-$confirmar_senha = $_POST['confirmar_senha'];
+	<?php
 
 
-echo "Nome: ".$nome ."<br>";
-echo "Email: ".$email ."<br>";
-echo "Senha: ".$senha ."<br>";
-echo "Confirmação: ".$confirmar_senha ."<br>";
-/*
-echo "Senha: ".$_POST["senha"]."<br>";
-echo "Email: ".$_POST["email"]."<br>";
-echo "Sexo: ".$_POST["sexo"]."<br>";
-echo "Telefone: ".$_POST["telefone"]."<br>";
-echo "Celular: ".$_POST["celular"]."<br>";
-echo "Endereço: ".$_POST["endereco"]."<br>";
-echo "Cidade: ".$_POST["cidade"]."<br>";
-echo "Estado: ".$_POST["estado"]."<br>"
-echo "Senha: ".$_POST["senha"]."<br>";
-echo "Mensagem: ".$_POST["mensagem"]."<br>";
-*/
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+	$senha = $_POST['senha'];
+	$confirmar_senha = $_POST['confirmar_senha'];
+	$sexo = $_POST['sexo'];
+	$telefone = $_POST['telefone'];
+	$celular = $_POST['celular'];
+	$endereco = $_POST['endereco'];
+	$cidade = $_POST['cidade'];
+	$estado = $_POST['estado'];
 
-?>
+
+	?>
+	<br /><br />
+	<div class="container">
+		<table border="1" class="table table-hover">
+			<thead>
+				<tr>
+					<th colspan="2">DADOS CADASTRADOS</th>      
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th>Nome</th>
+					<td><?php echo "$nome"; ?></td>
+				</tr>
+				<tr>
+					<th>E-mail</th>
+					<td><?php echo "$email"; ?></td>
+				</tr>
+				<tr>
+					<th>Senha</th>
+					<td><?php echo "$senha"; ?></td>
+				</tr>
+				<tr>
+					<th>Sexo</th>
+					<td><?php echo "$sexo"; ?></td>
+				</tr>
+				<tr>
+					<th>Telefone</th>
+					<td><?php echo "$telefone"; ?></td>
+				</tr>
+				<tr>
+					<th>Celular</th>
+					<td><?php echo "$celular"; ?></td>
+				</tr>
+				<tr>
+					<th>Endere&ccedilo</th>
+					<td><?php echo "$endereco"; ?></td>
+				</tr>
+				<tr>
+					<th>Cidade</th>
+					<td><?php echo "$cidade"; ?></td>
+				</tr>
+				<tr>
+					<th>Estado</th>
+					<td><?php echo "$estado"; ?></td>
+				</tr>
+			</tbody>
+		</table>
+
+		<?php
+		echo "<script>alert(\"Cadastro enviado com sucesso!\")</script>";
+		?>
+	</div>
+
+
 </body>
 </html>
